4c98056d845c1513968a8e9e07bbb1e1d6d74f1b
Renan Hatherly <renan.hath@gmail.com>
2024-04-30 16:52:53 -0300

    HyperMedia interfaces, response enricher, filters and constants
    - First implementation of the class library, with constants classes, interfaces, filters and response enricher.

---
+++ HATEOAS/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using HATEOAS.Hypermedia.Abstract;
+
+namespace HATEOAS.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
